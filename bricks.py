f09370cc092f66b27a494623bdc21dd7d07ae2a8
vinayak932 <vinayakd299@gmail.com>
2026-06-26 21:47:47 +0530

    Update bricks.py

--- bricks.py
+++ bricks.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 class Bricks(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
+        self. shape("square")
         self.color("red")
         self.shapesize(stretch_wid=1,stretch_len=2)
         self.penup()
